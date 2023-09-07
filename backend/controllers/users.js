5a6d008e350116c3663834b527081924f8759940
Alex <manicynaleksandr@yandex.ru>
2023-09-07 23:55:45 +0300

    token fix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -117,7 +117,7 @@ const login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: true,
+          // sameSite: true,
         })
         .send({
           name: user.name,
