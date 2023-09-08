7291bfdec485a2ee80b00a9854e072bc501c248c
Alex <manicynaleksandr@yandex.ru>
2023-09-08 20:59:24 +0300

    token fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -77,7 +77,7 @@ class Api {
 
 const api = new Api("https://rocket.api.nomoredomainsicu.ru", {
   "Content-Type": "application/json",
-  // authorization: `Bearer ${localStorage.getItem("jwt")}`,
+  authorization: `Bearer ${localStorage.getItem("jwt")}`,
 });
 
 export default api;
