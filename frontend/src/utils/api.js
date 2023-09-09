c1b9051dc64673ee48b615de1576bf61c51d0429
Alex <manicynaleksandr@yandex.ru>
2023-09-09 22:12:40 +0300

    retches fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,74 +6,68 @@ class Api {
     this._headers = headers;
   }
 
-  _request(url, options) {
-    return fetch(url, options).then(handleRequest);
-  }
-
   // Возвращает промис для получения первоначальных карточек с сервера
   getInitialCards() {
-    return this._request(`${this._baseUrl}/cards`, {
-      method: "GET",
+    return fetch(`${this._baseUrl}/cards`, {
       headers: this._headers,
-    });
+    }).then(handleRequest);
   }
 
   // Возвращает промис для первоначального получения данных профиля с сервера (имени, описания и аватара)
   getProfileInfo() {
-    return this._request(`${this._baseUrl}/users/me`, {
-      method: "GET",
+    return fetch(`${this._baseUrl}/users/me`, {
       headers: this._headers,
-    });
+    }).then(handleRequest);
   }
 
   // Возвращает промис для добавления новой карточки
   postNewCard({ name, link }) {
-    return this._request(`${this._baseUrl}/cards`, {
+    return fetch(`${this._baseUrl}/cards`, {
       method: "POST",
       headers: this._headers,
       body: JSON.stringify({
         name,
         link,
       }),
-    });
+    }).then(handleRequest);
   }
 
   // Возвращает промис для удаления карточки
   deleteCard(cardId) {
-    return this._request(`${this._baseUrl}/cards/${cardId}`, {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
       method: "DELETE",
       headers: this._headers,
-    });
+    }).then(handleRequest);
   }
 
   // Возвращает промис для установки новых данных профиля из формы
-  setProfileInfo({name, about}) {
-    return this._request(`${this._baseUrl}/users/me`, {
+  setProfileInfo({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         name,
         about,
       }),
-    });
+    }).then(handleRequest);
   }
 
   // Возвращает промис для установки нового аватара профиля
-  setProfileAvatar({avatar}) {
-    return this._request(`${this._baseUrl}/users/me/avatar`, {
+  setProfileAvatar({ avatar }) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         avatar,
       }),
-    });
+    }).then(handleRequest);
   }
 
   changeLikeCardStatus(cardId, isLiked) {
-    return this._request(`${this._baseUrl}/cards/${cardId}/likes`, {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
       method: isLiked ? "DELETE" : "PUT",
       headers: this._headers,
-    });
+    }).then(handleRequest);
   }
 }
 
